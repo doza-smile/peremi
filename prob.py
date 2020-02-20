357c62dd358da381db5923d30fea06559ece2320
doza-smile <doz1618@mail.ru>
2020-02-20 21:06:53 +0300

    prodol

---
+++ prob.py
@@ -0,0 +1,13 @@
+print("Hello!")
+print ("Hello!", "студентики,", "но только 1 и 4")
+
+print ("Hello!", "студентики,", "но только 1 и 4", sep = "xxx")
+
+print ("Hello!", "студентики,", "но только 1 и 4", end = "ppp")
+print ()
+# Ввод
+
+age = input("Input your age")
+
+print(age, type(age))
+print(int(age), type(int(age)))
